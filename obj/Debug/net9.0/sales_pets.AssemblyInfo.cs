8f9377734c3e063935c88cd81fa770ebea9ad765
96saw <sawehkalane_phaung@cmu.ac.th>
2024-11-17 19:20:41 +0700

    Add about us page

--- obj/Debug/net9.0/sales_pets.AssemblyInfo.cs
+++ obj/Debug/net9.0/sales_pets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sales_pets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7900885eecb4e1a7f7a61fec5fb5792895339a66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9dc3027487b26efaebde92970338d77d794135")]
 [assembly: System.Reflection.AssemblyProductAttribute("sales_pets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sales_pets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
